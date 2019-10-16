6108dae429e0e83e7bf01a18e1db0045e87ecc1e
HashimotoMasaru <43205425+HashimotoMasaru@users.noreply.github.com>
2019-10-16 18:05:58 +0900

    No1　内容追加 No2　ファイル追加

--- FlowChart/src/No1.java
+++ FlowChart/src/No1.java
@@ -1,9 +1,14 @@
 
-public class no1 {
+public class No1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		int a = 1;
+		int b = 2;
+		int c = a + b;
+
+		c = c + 1;
 	}
 
 }
